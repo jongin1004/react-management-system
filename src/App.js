54d06319d09d41548e438d86095f8ccf860b9341
jongin1004 <bill1224@naver.com>
2022-01-14 17:51:46 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-// import logo from './logo.svg';
+import React from 'react';
 import Customer from './components/Customer';
-import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -29,26 +46,38 @@ const customers = [
   },
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer 
-              key={c.id} //for문이나 map같은 다수의 정보를 다루는 경우는 key값을 정의해줘야합니다. 
-              id={c.id}
-              name={c.name}
-              image={c.image}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          )
-        })
-      }
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const {classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return (
+                <Customer 
+                key={c.id} //for문이나 map같은 다수의 정보를 다루는 경우는 key값을 정의해줘야합니다. 
+                id={c.id}
+                name={c.name}
+                image={c.image}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              /> )})}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
