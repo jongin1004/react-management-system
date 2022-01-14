cd9e456c823abb6cd6c353037a97df9bd650fa1d
jongin1004 <bill1224@naver.com>
2022-01-15 00:44:26 +0900

    Add CustomerAdd Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -72,38 +73,41 @@ class App extends React.Component {
   render() {
     const {classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c => { return (
-                <Customer 
-                key={c.id} //for문이나 map같은 다수의 정보를 다루는 경우는 key값을 정의해줘야합니다. 
-                id={c.id}
-                name={c.name}
-                image={c.image}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              /> )}):
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colspan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                </TableCell>
-              </TableRow>                
-              }
-          </TableBody>
-        </Table>
-      </Paper>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c => { return (
+                  <Customer 
+                  key={c.id} //for문이나 map같은 다수의 정보를 다루는 경우는 key값을 정의해줘야합니다. 
+                  id={c.id}
+                  name={c.name}
+                  image={c.image}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                /> )}):
+                <TableRow>
+                  <TableCell colspan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>                
+                }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd />
+      </div>
     );
   }
 }
---
+++ client/src/components/CustomerAdd.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { post } from 'axios'; //post 방식으로 고객추가 데이터를 보낼 수 있도록 
+
+class CustomerAdd extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: ''
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        this.addCustomer().then((res) => {console.log(res.data)});
+    }
+
+    handleFileChange = (e) => {
+        this.setState({
+            file: e.target.files[0],
+            fileName: e.target.value
+        })
+    }
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
+    addCustomer = () => {
+        const url = '/api/customers';
+        const formData = new FormData(); //form데이터를 전송하기 위한 객체 생성
+        formData.append('image', this.state.file);
+        formData.append('name', this.state.userName);
+        formData.append('birthday', this.state.birthday);
+        formData.append('gender', this.state.gender);
+        formData.append('job', this.state.job);
+
+        // 파일이 포함되어있는 data를 전송하기 위한 header 추가 
+        const config = {
+            header: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+        return post(url, formData, config);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleFormSubmit}>
+                <h1>고객 추가</h1>
+                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/>
+                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /> 
+                생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /> 
+                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} />   
+                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /> 
+                <input type="submit" value="추가하기" />
+            </form>
+        )
+    }
+}
+
+export default CustomerAdd
